6e042444674e63b8b8a554e19de098cb47abf6f4
elliotr1 <84349926+elliotr1@users.noreply.github.com>
2023-08-10 09:09:52 +0100

    I've implemented a function that returns the lowest variances within the testing dataset.
    I've added a function that calculates the variance uncertainty of the rows of unseen data. It then returns the bottom 10% variance row indexes.

--- Models/Naive_Bayes/Bernoulli/Testing/testing.py
+++ Models/Naive_Bayes/Bernoulli/Testing/testing.py
@@ -2,8 +2,7 @@ import numpy as np
 
 from Models.Naive_Bayes.Bernoulli.Testing.BernoulliCode.Bernoulli import bernoulli_naive_bayes_function
 from Models.Naive_Bayes.Bernoulli.Testing.laplace.laplace import getLapLaceTuningForcolumn
-spamVariance = []
-hamVariance = []
+
 def naiveBayesTestingFunc(training_probabilities, testing_set, columns, bias=1.3):
     overall_predictions = []
     for i in range(len(testing_set)):
@@ -16,10 +15,36 @@ def naiveBayesTestingFunc(training_probabilities, testing_set, columns, bias=1.3
                 word_list=word_list, training_probabilities=training_probabilities,
                 column_name=column, k=k, column_predictions=column_predictions,
                 word_probabilities=word_probabilities)
-        hamVariance.append(np.array(column_predictions["ham"]).var())
-        spamVariance.append(np.array(column_predictions["spam"]).var())
         if abs(sum(column_predictions["ham"])) * bias > abs(sum(column_predictions["spam"])):
             overall_predictions.append(1)
         else:
             overall_predictions.append(0)
     return overall_predictions
+
+
+
+def NaiveBayesTestingActiveLearning(training_probabilities, testing_set, columns, bias=1.3):
+    spamVariance = []
+    hamVariance = []
+    overall_predictions = []
+    for i in range(len(testing_set)):
+        row = testing_set.iloc[i]  # Access the row by positional index
+        column_predictions = {"spam": [], "ham": []}
+        for column in columns:
+            k, word_list = getLapLaceTuningForcolumn(row, column, training_probabilities)
+            word_probabilities = {"spam": [], "ham": []}
+            word_probabilities, column_predictions = bernoulli_naive_bayes_function(
+                word_list=word_list, training_probabilities=training_probabilities,
+                column_name=column, k=k, column_predictions=column_predictions,
+                word_probabilities=word_probabilities)
+        hamVariance.append({"Index": i, "variance": np.array(column_predictions["ham"]).var()})
+        spamVariance.append({"Index": i, "variance": np.array(column_predictions["spam"]).var()})
+        if abs(sum(column_predictions["ham"])) * bias > abs(sum(column_predictions["spam"])):
+            overall_predictions.append(1)
+        else:
+            overall_predictions.append(0)
+    hamVariance = sorted(hamVariance, key=lambda item: hamVariance["variance"])
+    spamVariance = sorted(spamVariance, key=lambda item: spamVariance["variance"])
+    top_bottom_10_percent_hamVariance = hamVariance[:int(math.floor(len(hamVariance)*.1))]
+    top_bottom_10_percent_spamVariance = spamVariance[:int(math.floor(len(spamVariance)*.1))]
+    return [overall_predictions, top_bottom_10_percent_hamVariance, top_bottom_10_percent_spamVariance]
